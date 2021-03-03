9c45a844f35c7c8ebed5511e3ca6ca71589f03ec
ChaonengTan <chaonengtan@gmail.com>
2021-03-02 16:43:55 -0800

    +editPlant

--- app.py
+++ app.py
@@ -104,18 +104,20 @@ def edit(plant_id):
     if request.method == 'POST':
         # TODO: Make an `update_one` database call to update the plant with the
         # given id. Make sure to put the updated fields in the `$set` object.
-        mongo.db.plants.update_one({'$set':{
+        searchParam = {'name':plant_id}
+        setParam = {'$set':{
             'name': request.form.get('plant_name'),
             'variety': request.form.get('variety'),
             'photo_url': request.form.get('photo'),
             'date_planted': request.form.get('date_planted')
-        }})
+        }}
+        mongo.db.plants.update_one(searchParam, setParam)
         
         return redirect(url_for('detail', plant_id=plant_id))
     else:
         # TODO: Make a `find_one` database call to get the plant object with the
         # passed-in _id.
-        plant_to_show = mongo.db.plants.find_one(plant_id)
+        plant_to_show = mongo.db.plants.find_one({'name':plant_id})
 
         context = {
             'plant': plant_to_show
